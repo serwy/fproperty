510d881b2db0f654b2bd3d77c2b05638fb4efd74
Roger D. Serwy <roger.serwy@gmail.com>
2020-08-08 18:34:14 -0500

    simplified decorator

--- fproperty/api.py
+++ fproperty/api.py
@@ -8,26 +8,7 @@ _property = builtins.property
 
 def fproperty(func):
     """Decorator for a function that returns (fget, fset, fdel, doc)"""
-    result = func()
-    if not isinstance(result, (tuple, list)):
-        raise ValueError('Expected tuple, not %s' % type(result))
-
-    fget = fset = fdel = doc = None
-    x = len(result)
-    if x == 0:
-        pass
-    elif x == 1:
-        fget, = result
-    elif x == 2:
-        fget, fset = result
-    elif x == 3:
-        fget, fset, fdel = result
-    elif x == 4:
-        fget, fset, fdel, doc = result
-    else:
-        raise ValueError('too many return values: %i' % x)
-
-    return _property(fget, fset, fdel, doc)
+    return _property(*func())
 
 
 fproperty.apply = fproperty
